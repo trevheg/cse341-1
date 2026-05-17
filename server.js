f9f50810545970ef69e8fbc41430d8f79f89f9fb
trevheg <trevorhegstrom@gmail.com>
2026-05-16 22:09:06 -0700

    9:21

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const open = require('open'); // not sure if I still need this
 const app = express();
+
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
     res.render('index'); 
 })
 
-app.listen(3000);
+app.listen(3000, async () => {
+  console.log('Server is running at http://localhost:3000');
+  const open = await import('open');
+  open.default('http://localhost:3000');
+});
